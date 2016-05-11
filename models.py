ac47172eb5f3c6880818b76089558feb0eef7be0
Camilo Forero <ce.forero2551@uniandes.edu.co>
2016-05-11 02:45:10 -0500

    Added some new functionality to the automatic emails

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class ODTTemplate(models.Model):
         This represents a template, which should contain the actual file and a name so this can be easily retrieved in the API
     """
     name = models.CharField(max_length=16, unique=True, db_index=True)
+    file_name = models.CharField(max_length=32, default="default")
     templateFile = models.FileField(upload_to=upload_prefix, help_text=u"Acá va el archivo de formato ODT que será utilizado para generar el PDF. Si quieres saber más revisa el booklet que se encuentra en esta dirección: LINK")
     def __str__(self):
         return self.name
