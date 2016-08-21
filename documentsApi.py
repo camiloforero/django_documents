e04d3a60b4c76c10d6c975a65faca77d1fbc9e78
Camilo Forero <ce.forero2551@uniandes.edu.co>
2016-08-20 19:34:59 -0500

    Agregada una opción para que el nombre de los documentos se pueda generar con lenguaje de template de django Agregada la posibilidad de pedir un documento odt directamente desde el metodo render

--- documentsApi.py
+++ documentsApi.py
@@ -13,16 +13,19 @@ class ODTTemplate():
         """
         self.template = webodt.ODFTemplate(templateName)
 
-    def render(self, context):
+    def render(self, context, odt=False):
         document = self.template.render(Context(context), delete_on_close=False)
-        oldenviron = os.environ['HOME']
-        os.environ['HOME'] = "/tmp"
-        returncode = subprocess.call(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", "/tmp", document.name])
-        print "PDF generation return code: %s " % str(returncode)
-        filename, file_extension = os.path.splitext(document.name)
-        pdf = open(filename + ".pdf")
-        os.environ['HOME'] = oldenviron
-        return pdf
+        if odt:
+            return document
+        else:
+            oldenviron = os.environ['HOME']
+            os.environ['HOME'] = "/tmp"
+            returncode = subprocess.call(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", "/tmp", document.name])
+            print "PDF generation return code: %s " % str(returncode)
+            filename, file_extension = os.path.splitext(document.name)
+            pdf = open(filename + ".pdf")
+            os.environ['HOME'] = oldenviron
+            return pdf
         
     def renderODT(self, context):
         document = self.template.render(Context(context), delete_on_close=False)
--- models.py
+++ models.py
@@ -4,14 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 def upload_prefix(instance, name):
-    return u"odtTemplates/%s.odt" % (instance.name, )
+    return u"odtTemplates/%s.odt" % (instance.file_name, )
 
 class ODTTemplate(models.Model):
     """
         This represents a template, which should contain the actual file and a name so this can be easily retrieved in the API
     """
-    name = models.CharField(max_length=16, unique=True, db_index=True)
-    file_name = models.CharField(max_length=32, default="default")
+    name = models.CharField("Nombre", max_length=16, unique=True, db_index=True, help_text="Un nombre único para este documento que sirva para identificarlo y diferenciarlo de los demás")
+    file_name = models.SlugField(max_length=32, default="default", help_text="El nombre que va a tener el archivo cuando se guarde")
+    generated_file_name = models.CharField("Nombre autogenerado", max_length=32, default="default", help_text="El nombre final que va a tener el documento cuando se envíe por correo o se suba a PODIO. Admite sintaxis de templates de Django para valores dinámicos")
     templateFile = models.FileField(upload_to=upload_prefix, help_text=u"Acá va el archivo de formato ODT que será utilizado para generar el PDF. Si quieres saber más revisa el booklet que se encuentra en esta dirección: LINK")
     def __str__(self):
         return self.name
